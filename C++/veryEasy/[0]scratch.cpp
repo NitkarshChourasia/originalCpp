76cecf2e8018fd25093bad0b76e2e312f37b4788
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-12-23 00:15:01 +0530

    newCommits @ 2022-12-23-00:15:01

--- C++/veryEasy/[0]scratch.cpp
+++ C++/veryEasy/[0]scratch.cpp
@@ -4,16 +4,17 @@ void greet()
 {
     cout << "Hello world";
 }
-
+void numbers(int inum1, float fnum2)
+{
+    cout << "The int number is " << inum1 << endl;
+    cout << "The float number is " << fnum2 << endl;
+}
 int main()
 {
     int n = 7;
     cout << n << endl;
+    // Calling the functions:
+    numbers(2, 232.131);
     greet();
     return 0;
 }
-
-void displayNum()
-
-
-
