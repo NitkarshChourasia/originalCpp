5a72396d2d93e0ee6743abce0c5b0bfc40ee23fc
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-12-23 14:45:01 +0530

    newCommits @ 2022-12-23-14:45:01

--- C++/veryEasy/[0]scratch.cpp
+++ C++/veryEasy/[0]scratch.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 using namespace std;
-
+int dataTypes();
 int main()
 {
     int x, y, sum;
@@ -10,5 +10,29 @@ int main()
     cin >> y;
     sum = x + y;
     cout << "The sum of x and y is: " << sum << endl;
+    cout << endl;
+    dataTypes();
     return 0;
+}
+
+int dataTypes()
+{
+    // Creating variables.
+    int int1 = 7;
+    float float1 = 14.31;
+    double double1 = 13.423;
+    char char1 = 'D';
+    string string1 = "Hello World!";
+    bool bool1 = true;
+    bool bool2 = false;
+
+    // Printing the variables.
+    cout << "Int is: " << int1 << "\n";
+    cout << "Float is: " << float1 << "\n";
+    cout << "Double is: " << double1 << "\n";
+    cout << "Char is: " << char1 << "\n";
+    cout << "String is: " << string1 << "\n";
+    cout << "Bool #1 is: " << bool1 << "\n";
+    cout << "Bool #2 is: " << bool2 << "\n";
+
 }
