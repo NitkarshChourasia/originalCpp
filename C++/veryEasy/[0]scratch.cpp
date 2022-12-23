a69401c22a7d2186c72cd357bd6d1b11dc03edb9
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-12-23 13:30:01 +0530

    newCommits @ 2022-12-23-13:30:01

--- C++/veryEasy/[0]scratch.cpp
+++ C++/veryEasy/[0]scratch.cpp
@@ -2,43 +2,45 @@
 #include <cmath>
 using namespace std;
 
-int add(int int1, int int2);
-int sqrt(int int1);
-int substract(int int1, int int2);
-int multiply(int int1, int int2);
-int divide(int int1, int int2);
+// int add(int int1, int int2);
+// int sqrt1(int int1);
+// int substract(int int1, int int2);
+// int multiply(int int1, int int2);
+// int divide(int int1, int int2);
 
-int main()
-{
-    cout << add(100, 100) << endl;
-    cout << substract(100, 50) << endl;
-    cout << multiply(100, 100) << endl;
-    cout << divide(100, 50) << endl;
-    cout <<sqrt(100) << endl;
-}
+// int main()
+// {
+// #cout << add(100, 100) << endl;
+// #cout << substract(100, 50) << endl;
+// #cout << multiply(100, 100) << endl;
+// #cout << divide(100, 50) << endl;
+// #cout <<sqrt1(100) << endl;
+// }
 
 int add(int int1, int int2)
 {
     return int1 + int2;
 }
 
-int substract(int int1, int int2)
-{
-    cout << int1 - int2 << endl;
-    return int2 - int1;
-}
+add(2, 2)
 
-int divide(int int1, int int2)
-{
-    cout << int1 / int2 << endl;
-    return int2 / int1;
-}
-
-int multiply(int int1, int int2)
-{
-    return int1 * int2;
-}
-int sqrt(int int1)
-{
-    return sqrt(int1);
-}
+// int substract(int int1, int int2)
+// {
+//     cout << int1 - int2 << endl;
+//     return int2 - int1;
+// }
+// 
+// int divide(int int1, int int2)
+// {
+//     cout << int1 / int2 << endl;
+//     return int2 / int1;
+// }
+// 
+// int multiply(int int1, int int2)
+// {
+//     return int1 * int2;
+// }
+// int sqrt1(int int1)
+// {
+//     return sqrt(int1);
+// }
