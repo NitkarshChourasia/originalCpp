b270619c4ebe7c59d1b276cec5b20f3cfa0e0de7
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-12-23 14:30:01 +0530

    newCommits @ 2022-12-23-14:30:01

--- C++/veryEasy/[0]scratch.cpp
+++ C++/veryEasy/[0]scratch.cpp
@@ -3,6 +3,12 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    int x, y, sum;
+    cout << "Type a number: ";
+    cin >> x;
+    cout << "Type another number: ";
+    cin >> y;
+    sum = x + y;
+    cout << "The sum of x and y is: " << sum << endl;
     return 0;
 }
