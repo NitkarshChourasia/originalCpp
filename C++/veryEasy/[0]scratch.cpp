2824523e76aaf16add8003b6cab67ccdb9a8996f
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2022-12-23 18:00:01 +0530

    newCommits @ 2022-12-23-18:00:01

--- C++/veryEasy/[0]scratch.cpp
+++ C++/veryEasy/[0]scratch.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
 using namespace std;
 int dataTypes();
+int add(int int1, int int2);
+
+int switchCases(int int1);
+
+
+
 int main()
 {
-    int x, y, sum;
-    cout << "Type a number: ";
-    cin >> x;
-    cout << "Type another number: ";
-    cin >> y;
-    sum = x + y;
-    cout << "The sum of x and y is: " << sum << endl;
+    switchCases(10);
     cout << endl;
     dataTypes();
     return 0;
@@ -36,3 +36,46 @@ int dataTypes()
     cout << "Bool #2 is: " << bool2 << "\n";
 
 }
+
+    int add(int int1, int int2)
+    {
+    int x, y, sum;
+    cout << "Type a number: ";
+    cin >> x;
+    cout << "Type another number: ";
+    cin >> y;
+    sum = x + y;
+    cout << "The sum of x and y is: " << sum << endl;
+    cout << endl;
+    }
+
+    int switchCases(int int1)
+    {
+        switch(int1){
+            case 1:
+                cout << "Monday";
+                break;
+            case 2:
+                cout << "Tuesday";
+                break;
+            case 3:
+                cout << "Wednesday";
+                break;
+            case 4:
+                cout << "Thursday";
+                break;
+            case 5:
+                cout << "Friday";
+                break;
+            case 6:
+                cout << "Saturday";
+                break;
+            case 7:
+                cout << "Sunday";
+                break;
+            default:
+                cout << "Chal, Hath behen ki lodi. Chal hath behen ki lodi";
+                break;
+        }
+        return 0;
+    }
