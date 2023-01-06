2dc91fe5f39ba056c03f016427da32461b14d923
Nitkarsh Chourasia <playnitkarsh@gmail.com>
2023-01-06 16:30:01 +0530

    newCommits @ 2023-01-06-16:30:01

---
+++ C++/veryEasy/scratchFiles/[1]scratch.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    cout << "Please enter your name: ";
+    string name;
+    cin >> name;
+    cout << "Hello %s" % name << endl;
+    return 0;
+}
+
+// Fuck can't even solve this things properly.
